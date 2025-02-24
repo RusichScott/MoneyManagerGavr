9c5d4b4f8cd71bb457074be08473e15370403c25
RusichScott <dimagavrilovskij5@gmail.com>
2025-02-24 14:41:10 +0300

    Добавьте файлы проекта.

---
+++ View/LoginWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoneyManagerGavr.View
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Логика входа в систему
+            ManagerWindow managerWindow = new ManagerWindow();
+            MessageBox.Show("Вы успешно вошли в систему!");
+            managerWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ View/ManagerWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoneyManagerGavr.View
+{
+    /// <summary>
+    /// Логика взаимодействия для ManagerWindow.xaml
+    /// </summary>
+    public partial class ManagerWindow : Window
+    {
+        public ManagerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Image_Click(object sender, MouseButtonEventArgs e)
+        {
+            ManagerFrame.Content = new HomePage();
+        }
+
+        private void Income_Image_Click(object sender, MouseButtonEventArgs e)
+        {
+            ManagerFrame.Content = new IncomePage();
+        }
+
+        private void Spendings_Image_Click(object sender, MouseButtonEventArgs e)
+        {
+            ManagerFrame.Content = new SpendingsPage();
+        }
+
+        private void Account_Image_Click(object sender, MouseButtonEventArgs e)
+        {
+            ManagerFrame.Content = new AccountPage();
+        }
+
+        private void Settings_Image_Click(object sender, MouseButtonEventArgs e)
+        {
+            ManagerFrame.Content = new SettingsPage();
+        }
+    }
+}
